2e50aa796f18991932c66268332d8a48ad550b07
tharsila <tharsilapaula@gmail.com>
2022-07-01 21:01:00 -0300

    feat: migration for users

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,11 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'cpf',
+        'city',
+        'state',
+        'country',
+        'userType'
     ];
 
     /**
