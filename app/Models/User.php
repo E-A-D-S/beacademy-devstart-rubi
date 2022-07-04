faa39e76fb2ed44fc41cb64ca473f259a0f02128
Elivandro <96877171+Elivandro@users.noreply.github.com>
2022-07-04 10:57:16 -0400

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,7 +23,9 @@ class User extends Authenticatable
         'email',
         'password',
         'cpf',
-        'userType'
+        'userType',
+        'created_at',
+        'updated_at'
     ];
 
     /**
