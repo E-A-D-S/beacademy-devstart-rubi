1a72e36a7188f13200fde387ea283c4ed4701280
Elivandro <96877171+Elivandro@users.noreply.github.com>
2022-07-04 21:04:07 -0400

    Update login.blade.php
    adicionada rota para criar cadastro

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -2,51 +2,41 @@
 @extends('title', 'titulo da página')
 
 @section('content')
-
 <main class="content text-light p-5">
-
-<div>
   <div class="modal-dialog" role="document">
     <div class="modal-content style="background-color: #efefef;>
-    <div class="modal-body text-dark">
-      <h1 class="mb-3">Login</h1>
-
-        <form class="pl-4 pr-4">
-          @csrf
-    <div class="form-group">
-        <label>Email</label>
-        <input type="Email" class="form-control" placeholder="Digite seu email">
-    </div>
-     <div class="form-group">
-        <label>Senha</label>
-          <a href="#" class="float-right">
-            <i class="fa fa-eye"></i>
-          </a>
-          <input type="Senha" class="form-control" placeholder="Digite sua senha">
-     </div>
-
-          <button type="submit" class="btn btn-primary btn-block" >Entrar</button>
-
-    <div class="row mt-3">
-    <div class="col">
-          <input type="checkbox"
-            name="remember"
-            id="remember"/><label
-            for="remember">Lembrar-me</label>
+        <div class="modal-body text-dark">
+        <form active="" method="post" class="pl-4 pr-4">
+            @csrf
+            <h1 class="mb-3">Login</h1>
+            <div class="form-group">
+                <label>Email</label>
+                <input type="Email" class="form-control" placeholder="Digite seu email">
+            </div>
+            <div class="form-group">
+                <label>Senha</label>
+                    <a href="#" class="float-right">
+                        <i class="fa fa-eye"></i>
+                    </a>
+                <input type="Senha" class="form-control" placeholder="Digite sua senha">
+            </div>
+            <button type="submit" class="btn btn-primary btn-block" >Entrar</button>
+            <div class="row mt-3">
+                <div class="col">
+                    <input type="checkbox" name="remember" id="remember"/>
+                    <label for="remember">Lembrar-me</label>
+                </div>
+                <div class="col">
+                        <a href="#">Esqueci minha senha</a>
+                </div>
+            </div>
+            <div class="text-center mt-4 mb-1">
+                <span>Não possui cadastro? </span>&nbsp;
+                <a href="{{ route('users.create') }}">Cadastre-se aqui</a>
+            </div>
+        </form>
         </div>
-
-     <div class="col">
-             <a href="#">Esqueci minha senha</a>
-      </div>
-      </div>
-       <div class="text-center mt-4 mb-1">
-          <span>Não possui cadastro? </span>&nbsp;
-          <a href="#">Cadastre-se aqui</a>
-      </div>
-      </form>
-      </div>
       </div>
     </div>
-</div>
 </main
 @endsection
