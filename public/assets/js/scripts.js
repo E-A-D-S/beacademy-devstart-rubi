b26462e574fd30e52845c4eba7ac5f9a42516a84
Renata Deodório <renatadeod@gmail.com>
2022-07-06 12:36:34 -0300

    Ajustes bugfixForms

---
+++ public/assets/js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  $(".togglePassword").on("click", function(e) {
+    e.preventDefault()
+
+    var input_group = $(this).closest('.input-group ')
+    var input = input_group.find('input.form-control')
+    var icon = input_group.find('i')
+
+    input.attr('type', input.attr("type") === "text" ? 'password' : 'text')
+
+    icon.toggleClass('fa-eye-slash fa-eye')
+  })
+})
