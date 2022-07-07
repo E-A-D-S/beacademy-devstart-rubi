88dca626727043d34a53c70cdc650934ed2c6358
Elivandro <96877171+Elivandro@users.noreply.github.com>
2022-07-07 13:48:35 -0400

    Update UserFactory.php
    remoção de comentarios

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,32 +21,19 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'birthday' => $this->faker->date(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
             'cpf' => $this->faker->unique()->numerify('###########'),
+            'remember_token' => Str::random(10),
+
             'userType' => 0 ,
             'created_at' => now(),
             'updated_at' => now(),
-            'birthday' => $this->faker->date(),
+
 
         ];
     }
-    /* 
-     'id',
-        'name',
-        'email',
-        'password',
-        'cpf',
-        'userType',
-        'created_at',
-        'updated_at'
-    */
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return static
-     */
     public function unverified()
     {
         return $this->state(function (array $attributes) {
