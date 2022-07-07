71743f62d64b4bf6f3c7031b4520f39d7b818214
Elivandro <96877171+Elivandro@users.noreply.github.com>
2022-07-07 13:53:24 -0400

    Update contact.blade.php
    remoção de comentarios e uns espaços

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,13 +4,9 @@
 
 @section('content')
 
-
 <main class="container w-50 mt-5">
-
   <div class="modal-dialog" role="document">
-
     <div class="modal-content" style="background-color: #efefef;">
-
         <div class="modal-body text-dark">
 
     <h1 class="mt-3 mb-3">Entre em contato</h1>
@@ -35,29 +31,4 @@
     </div>
 </main>
 
-<!--<div class="container w-50 mt-5" style="background-color: #efefef;">
-    <div class="row">
-        <div class="col-md-12" >
-            <h1>Contato</h1>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-12">
-            <form action="">
-                <div class="form-group">
-                    <label for="nome">Nome</label>
-                    <input type="text" class="form-control" id="nome" placeholder="Nome">
-                </div>
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input type="email" class="form-control" id="email" placeholder="Email">
-                </div>
-                <div class="form-group">
-                    <label for="mensagem">Mensagem</label>
-                    <textarea class="form-control" id="mensagem" rows="3"></textarea>
-                </div>
-                <button type="submit" class="btn btn-primary">Enviar</button>
-            </form>
-        </div>
-    </div>-->
 @endsection
