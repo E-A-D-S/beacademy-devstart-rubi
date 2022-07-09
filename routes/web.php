4ed13180b2f3b6f7ca51998b0466b49222bfdbfb
Matheus Gomes <math.s.gomes@gmail.com>
2022-07-09 00:36:14 -0300

    Correction name atribute of routes, from product to products

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [IndexController::class, "index"])->name("index.index");
 Route::get('cadastro', [UserController::class, "index"])->name("users.create");
 
-Route::get('/produtos', [ProductController::class, 'index'])->name('product.index');
-Route::get('/produtos/novo', [ProductController::class, 'create'])->name('product.create');
-Route::post('/produto', [ProductController::class, 'store'])->name('product.store');
-Route::get('/produtos/{id}', [ProductController::class, 'show'])->name('product.show');
-Route::get('/produtos/{id}', [ProductController::class, 'update'])->name('product.update');
-Route::put('/produtos/{id}/editar', [ProductController::class, 'edit'])->name('product.edit');
-Route::delete('/produtos/{id}', [ProductController::class, 'destroy'])->name('product.destroy');
+Route::get('/produtos', [ProductController::class, 'index'])->name('products.index');
+Route::get('/produtos/novo', [ProductController::class, 'create'])->name('products.create');
+Route::post('/produto', [ProductController::class, 'store'])->name('products.store');
+Route::get('/produtos/{id}', [ProductController::class, 'show'])->name('products.show');
+Route::get('/produtos/{id}/editar', [ProductController::class, 'edit'])->name('products.edit');
+Route::put('/produtos/{id}', [ProductController::class, 'update'])->name('products.update');
+Route::delete('/produtos/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
