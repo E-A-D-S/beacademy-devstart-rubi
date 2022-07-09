a4cfa9a27360eab4c8d2dbcf72295c11db5eda4e
Matheus Gomes <math.s.gomes@gmail.com>
2022-07-09 00:18:15 -0300

    Route list products

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use App\Http\Controllers\{
   IndexController,
-  UserController
+  UserController,
+  ProductController
 };
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [IndexController::class, "index"])->name("index.index");
 Route::get('cadastro', [UserController::class, "index"])->name("users.create");
+
+Route::get('/produtos', [ProductController::class, 'index'])->name("product.index");
