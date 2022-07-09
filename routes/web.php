f57fcb499921adf92edf5015e87c0ed75180fc6c
Matheus Gomes <math.s.gomes@gmail.com>
2022-07-09 00:22:04 -0300

    Route edit product

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [IndexController::class, "index"])->name("index.index");
 Route::get('cadastro', [UserController::class, "index"])->name("users.create");
 
-Route::get('/produtos', [ProductController::class, 'index'])->name("product.index");
-Route::get('/produtos/novo', [ProductController::class, 'create'])->name("product.create");
+Route::get('/produtos', [ProductController::class, 'index'])->name('product.index');
+Route::get('/produtos/novo', [ProductController::class, 'create'])->name('product.create');
 Route::get('/produtos/{id}', [ProductController::class, 'update'])->name('product.update');
+Route::get('/produtos/{id}/editar', [ProductController::class, 'edit'])->name('product.edit');
