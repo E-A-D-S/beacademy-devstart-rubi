328d365c38dc05ffa01fb5adc1d4d5afe5a79308
Matheus Gomes <math.s.gomes@gmail.com>
2022-07-09 00:33:01 -0300

    Create function that show a single product

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ Route::get('cadastro', [UserController::class, "index"])->name("users.create");
 Route::get('/produtos', [ProductController::class, 'index'])->name('product.index');
 Route::get('/produtos/novo', [ProductController::class, 'create'])->name('product.create');
 Route::post('/produto', [ProductController::class, 'store'])->name('product.store');
+Route::get('/produtos/{id}', [ProductController::class, 'show'])->name('product.show');
 Route::get('/produtos/{id}', [ProductController::class, 'update'])->name('product.update');
 Route::put('/produtos/{id}/editar', [ProductController::class, 'edit'])->name('product.edit');
 Route::delete('/produtos/{id}', [ProductController::class, 'destroy'])->name('product.destroy');
