17e4a9359263553b6048f2022f2779b797a2787c
Matheus Gomes <math.s.gomes@gmail.com>
2022-07-09 00:18:57 -0300

    Route create a new product

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,4 @@ Route::get('/', [IndexController::class, "index"])->name("index.index");
 Route::get('cadastro', [UserController::class, "index"])->name("users.create");
 
 Route::get('/produtos', [ProductController::class, 'index'])->name("product.index");
+Route::get('/produtos/novo', [ProductController::class, 'create'])->name("product.create");
