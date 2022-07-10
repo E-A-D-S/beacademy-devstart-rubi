d10bbd39578da36b641f8eb30b455060d35b70ba
tharsila <tharsilapaula@gmail.com>
2022-07-09 21:28:13 -0300

    feat: Order model

---
+++ app/Models/Order.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'name',
+        'category',
+        'quantity',
+        'cost_price',
+        'sale_price',
+        'image',
+        'created_at',
+        'updated_at',
+    ];
+}
