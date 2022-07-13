33c4fb0145d9923c87ffde65086261058bbb250b
Elivandro <96877171+Elivandro@users.noreply.github.com>
2022-07-13 18:43:23 -0400

    Update register.blade.php

--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -9,7 +9,7 @@
             </div>
         @endif
 
-        @if($errors->any()))
+        @if($errors->any())
             @foreach($errors->all() as $error)
             <div class="alert alert-primary" role="alert">
                 {{ $error }}
