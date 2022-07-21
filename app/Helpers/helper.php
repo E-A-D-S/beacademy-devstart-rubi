b90ff6ee3e1e2f39ab014c032c2825452711fb31
Elivandro <Elivandro@hotmail.com.br>
2022-07-21 00:03:09 -0400

    função helper adicionado

---
+++ app/Helpers/helper.php
@@ -0,0 +1,39 @@
+<?php
+
+    function formatDateTime($datetime)
+    {
+        return \Carbon\Carbon::createFromFormat('Y-m-d', $datetime)->format('d/m/Y');
+    }
+
+    function formatCnpjCpf($docNumber)
+    {
+        $CPF_LENGTH = 11;
+        $cnpj_cpf = preg_replace("/\D/", '', $docNumber);
+
+        if (strlen($cnpj_cpf) === $CPF_LENGTH) {
+            return preg_replace("/(\d{3})(\d{3})(\d{3})(\d{2})/", "\$1.\$2.\$3-\$4", $cnpj_cpf);
+        } 
+
+        return preg_replace("/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/", "\$1.\$2.\$3/\$4-\$5", $cnpj_cpf);
+    }
+
+    function formatPhoneNumber($phoneNumber)
+    {
+        $celPhone  = 11;
+        $phone      = preg_replace("/\D/", "", $phoneNumber);
+
+        if(strlen($phone) === $celPhone){
+            return preg_replace("/(\d{2})(\d{1})(\d{4})(\d{2})/", "(\$1) \$2 \$3-\$4", $phone);
+        }
+
+        return preg_replace("/(\d{2})(\d{2})(\d{2})/", "(\$1) \$2\$3-", $phone);
+
+    }
+
+    function formatCep($cepNumber)
+    {
+        $cep      = preg_replace("/\D/", "", $cepNumber);
+
+        return preg_replace("/(\d{5})(\d{3})/", "\$1-\$2", $cep);
+
+    }
