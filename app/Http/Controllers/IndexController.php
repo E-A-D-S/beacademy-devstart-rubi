6ac1bbe06c44707c75dc8f20ca2365046cd65b0f
Matheus Gomes <math.s.gomes@gmail.com>
2022-07-20 22:27:19 -0300

    Product List Index

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,13 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\{
+    Product
+};
 
 class IndexController extends Controller
 {
 
     public function index()
     {
-        return view("index");
+        // return view("index");
+        $products = Product::all();
+        return view('index', compact('products'));
     }
     public function contact()
     {
