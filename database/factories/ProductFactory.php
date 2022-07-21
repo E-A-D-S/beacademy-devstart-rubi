c5ceef903e0823ffcd6e9fd6594ef2940effc87d
Elivandro <Elivandro@hotmail.com.br>
2022-07-21 14:26:59 -0400

    hotfix/factories

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -19,10 +20,10 @@ class ProductFactory extends Factory
         return [
             'name' => $this->faker->name(),
             'description' => $this->faker->paragraph(),
-            'category' => $this->faker->name(),
+            'category_id' => Category::All()->random()->id,
             'quantity' => $this->faker->numberBetween(1, 99),
-            'cost_price' => $this->faker->randomFloat(2, 00.00, 99.99),
-            'sale_price' => $this->faker->randomFloat(2, 00.00, 99.99),
+            'cost_price' => $this->faker->randomFloat(2, 100, 900),
+            'sale_price' => $this->faker->randomFloat(2, 1000, 2.000),
             'created_at' => now(),
             'updated_at' => now(),
         ];
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,10 +23,8 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'birthday' => $this->faker->date(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
-            'is_admin' => 0,
             'cpf' => $this->faker->unique()->numerify('###########'),
             'remember_token' => Str::random(10),
-            'userType' => 0 ,
             'created_at' => now(),
             'updated_at' => now(),
         ];
