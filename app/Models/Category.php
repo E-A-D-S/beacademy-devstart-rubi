80a1a77640f10636edc669c3c8087b85bc4f9e66
Elivandro <Elivandro@hotmail.com.br>
2022-07-21 14:27:42 -0400

    hotfix/model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,12 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model{
     use HasFactory;
-    
+
     protected $fillable = [
         'id',
         'name'
     ];
-    
+
     public function products()
     {
         return $this->belongsTo(Product::class);
