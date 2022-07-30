4f59fb1a3c78ee387c4524a175dfb057bd1e526c
Matheus Gomes <math.s.gomes@gmail.com>
2022-07-29 21:10:28 -0300

    Test: Index Page

--- tests/Feature/IndexTest.php
+++ tests/Feature/IndexTest.php
@@ -3,19 +3,14 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class IndexTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_the_application_returns_a_successful_response()
+    public function test_if_home_page_list_products()
     {
         $response = $this->get('/');
-
         $response->assertStatus(200);
     }
 }
