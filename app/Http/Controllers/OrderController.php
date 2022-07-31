c530d9f2739ba0b7dea0a8ffb201fb33337f3658
Elivandro <Elivandro@hotmail.com.br>
2022-07-31 13:48:12 -0400

    some fixes

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Order;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
 
 class OrderController extends Controller
 {
@@ -63,6 +64,57 @@ class OrderController extends Controller
     public function checkout()
     {
         return view('checkout');
+    }
+
+    public function payments(Request $request)
+    {
+        $data = $request->all();
+        dd($data);
+        
+        $data = [];
+        
+        $response = Http::withHeaders([
+            'X-First' => 'foo',
+            'X-Second' => 'bar'
+        ])->post('https://tracktools.vercel.app/api/checkout', [
+            'name' => 'Taylor',
+        ]);
+
+
+        // curl --request POST \
+        // --url https://tracktools.vercel.app/api/checkout \
+        // --header 'Content-Type: application/json' \
+        // --header 'token: UGFyYWLDqW5zLCB2b2PDqiBlc3RhIGluZG8gYmVtIQ==' \
+        // --data '{
+        // "transaction_type": "ticket",
+        // "transaction_amount": 129.48,
+        // "transaction_installment": 2,
+        // "customer_name": "Regis Santos",
+        // "customer_document":"33355566677",
+        // "customer_postcode": "12999444",
+        // "customer_address_street": "Rua dos Bobos",
+        // "customer_andress_number": "0",
+        // "customer_address_neighborhood": "Muito engraçada",
+        // "customer_address_city": "Caçapava",
+        // "customer_address_state": "São Paulo",
+        // "customer_address_country": "Brasil"
+        // }
+
+
+        // curl --request POST \
+        // --url https://tracktools.vercel.app/api/checkout \
+        // --header 'Content-Type: application/json' \
+        // --header 'token: UGFyYWLDqW5zLCB2b2PDqiBlc3RhIGluZG8gYmVtIQ==' \
+        // --data '{
+        // "transaction_type": "card",
+        // "transaction_amount": 129.48,
+        // "transaction_installments": 2,
+        // "customer_name": "Regis Santos",
+        // "customer_document":"33355566677",
+        // "customer_card_number": "3333444455556666",
+        // "customer_card_expiration_date": "12/2025",
+        // "customer_card_cvv": "892"
+        // }'
     }
 
 }
