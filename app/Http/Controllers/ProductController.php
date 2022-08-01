8c2676df1dcb7e9a2dced3be3e36924fe889bef0
Matheus Gomes <math.s.gomes@gmail.com>
2022-07-31 22:46:16 -0300

    Bugix slash

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -28,7 +28,7 @@ class ProductController extends Controller
         
         if($request->image){
             $file = $request['image'];
-            $path = $file->store('/assets/images/products/wine/', 'public');
+            $path = $file->store('assets/images/products/wine/', 'public');
             $data['image'] = $path;
         }
         $this->product->create($data);
