97947f06c075eb9a3fc9654a83efd80ee487b209
Matheus Gomes <math.s.gomes@gmail.com>
2022-08-02 00:16:37 -0300

    Add personalized message of payment approved for user

--- resources/views/orders/paid.blade.php
+++ resources/views/orders/paid.blade.php
@@ -2,15 +2,25 @@
 @section('title', 'Checkout dados para pagamento')
 
 @section('content')
+{{-- {{ dd($response['body']['transaction_type']) }} --}}
+
 <main class="container w-50 mt-5 mb-5">
   <div class="modal-content" style="background-color: #efefef;">
     <div class="modal-body text-dark">
       <div class="row">
         <div class="col-md-12 p-5">
-          <h1 class="text-center mb-4">Pagamento confirmado</h1>
+          <h1 class="text-center mb-4">
+            Pagamento via 
+            @if($response['body']['transaction_type'] == 'ticket')
+              boleto
+            @else
+              cartão de crédito
+            @endif
+            aprovado
+          </h1>
           <div class="text-center">
               <div>
-                  <h4 class=" mb-3">Clique abaixo e confira todos os pedidos</h4>
+                  <h4 class=" mb-3">Clique abaixo e confira todos os seus pedidos</h4>
               </div>
               <a href="{{ route('orders.details') }}" class="btn btn-success">Conferir Pedidos</a>
           </div>
